5e98c5b3124989f6b0a1cfb17b7c826b1d6d1b73
hyoni <165775769+Hyoni01@users.noreply.github.com>
2026-07-12 23:57:17 +0900

    add work: work-1783868232898

--- js/works.js
+++ js/works.js
@@ -22,6 +22,19 @@
 
 const WORKS = [
   {
+    id: "work-1783868232898",
+    category: "cha-personal",
+    title: "시계",
+    title_en: "clock",
+    year: 2026,
+    order: 1783868235379,     
+    thumb: "img/works/work-1783868232898/01.webp",
+    images: ["img/works/work-1783868232898/01.webp"],
+    tags: [],
+    description: "",
+    description_en: "",
+  },
+  {
     id: "work-1783868188251",
     category: "cha-personal",
     title: "광대",
